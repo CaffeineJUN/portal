af3be9b94c917232f8226de29f8f3fb0cb115b1f
caffeinejun <gurwns232e@gmail.com>
2022-04-02 17:06:34 +0900

    0401

--- userdao/src/main/java/kr/ac/jejunu/DaoFactory.java
+++ userdao/src/main/java/kr/ac/jejunu/DaoFactory.java
@@ -1,11 +1,43 @@
 package kr.ac.jejunu;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.logging.Logger;
+
+@Configuration
 public class DaoFactory {
-    public UserDao getUserDao() {
-        return new UserDao(getConnectionMaker());
+    @Value("${db.drivername}")
+    private String driverClassName;
+    @Value("${db.url}")
+    private String url;
+    @Value("${db.username}")
+    private String username;
+    @Value("${db.password}")
+    private String password;
+
+
+    @Bean
+    public UserDao userDao() throws ClassNotFoundException {
+        return new UserDao(dataSource());
     }
 
-    public ConnectionMaker getConnectionMaker() {
-        return new JejuConnectionMaker();
+    @Bean
+    public DataSource dataSource() throws ClassNotFoundException {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+
+        dataSource.setDriverClass((Class<? extends Driver>) Class.forName(driverClassName));
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
     }
 }
--- userdao/src/main/java/kr/ac/jejunu/UserDao.java
+++ userdao/src/main/java/kr/ac/jejunu/UserDao.java
@@ -1,63 +1,170 @@
 package kr.ac.jejunu;
 
+import javax.sql.DataSource;
 import java.sql.*;
 
 public class UserDao {
-    private final ConnectionMaker connectionMaker;
+    private final DataSource dataSource;
 
-    public UserDao(ConnectionMaker connectionMaker) {
-        this.connectionMaker = connectionMaker;
+    public UserDao(DataSource dataSource) {
+        this.dataSource = dataSource;
     }
 
     public User findById(Integer id) throws ClassNotFoundException, SQLException {
-        Connection connection = connectionMaker.getConnection();
-        //sql 작성
-        PreparedStatement preparedStatement =
-                connection.prepareStatement(
-                        "select * from userinfo where id = ?"
-                );
-        preparedStatement.setInt(1, id);
-        //sql 실행
-        ResultSet resultSet = preparedStatement.executeQuery();
-        resultSet.next();
-        //User 에 데이터 매핑
-        User user = new User();
-        user.setId(resultSet.getInt("id"));
-        user.setName(resultSet.getString("name"));
-        user.setPassword(resultSet.getString("password"));
-        //자원 해지
-        resultSet.close();
-        preparedStatement.close();
-        connection.close();
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        User user = null;
+        try{
+            connection = dataSource.getConnection();
+            //sql 작성
+            preparedStatement = connection.prepareStatement(
+                    "select * from userinfo where id = ?"
+            );
+            preparedStatement.setInt(1, id);
+            //sql 실행
+            resultSet = preparedStatement.executeQuery();
+            if(resultSet.next()) {
+                //User 에 데이터 매핑
+                user = new User();
+                user.setId(resultSet.getInt("id"));
+                user.setName(resultSet.getString("name"));
+                user.setPassword(resultSet.getString("password"));
+            }
+        }finally {
+            //자원 해지
+            try {
+                resultSet.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            try {
+                preparedStatement.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            try {
+                connection.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
         //User 리턴
         return user;
     }
 
 
     public void insert(User user) throws ClassNotFoundException, SQLException {
-        //드라이버 로딩
-        Connection connection = connectionMaker.getConnection();
-        //sql 작성
-        PreparedStatement preparedStatement =
-                connection.prepareStatement(
-                        "insert into userinfo(name, password) values ( ?, ? )"
-                        ,Statement.RETURN_GENERATED_KEYS
-                );
-        preparedStatement.setString(1, user.getName());
-        preparedStatement.setString(2, user.getPassword());
-        //sql 실행
-        preparedStatement.executeUpdate();
-        ResultSet resultSet = preparedStatement.getGeneratedKeys();
-        resultSet.next();
-        //User 에 데이터 매핑
-        user.setId(resultSet.getInt(1));
-        //자원 해지
-        resultSet.close();
-        preparedStatement.close();
-        connection.close();
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        try {
+            //드라이버 로딩
+            connection = dataSource.getConnection();
+            //sql 작성
+            preparedStatement = connection.prepareStatement(
+                    "insert into userinfo(name, password) values ( ?, ? )"
+                    , Statement.RETURN_GENERATED_KEYS
+            );
+            preparedStatement.setString(1, user.getName());
+            preparedStatement.setString(2, user.getPassword());
+            //sql 실행
+            preparedStatement.executeUpdate();
+            resultSet = preparedStatement.getGeneratedKeys();
+            resultSet.next();
+            //User 에 데이터 매핑
+            user.setId(resultSet.getInt(1));
+        } finally {
+            //자원 해지
+            try {
+                resultSet.close();
+            } catch (Exception throwables) {
+                throwables.printStackTrace();
+            }
+            try {
+                preparedStatement.close();
+            } catch (Exception throwables) {
+                throwables.printStackTrace();
+            }
+            try {
+                connection.close();
+            } catch (Exception throwables) {
+                throwables.printStackTrace();
+            }
+        }
+
     }
 
+    public void update(User user) throws SQLException {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        try {
+            //드라이버 로딩
+            connection = dataSource.getConnection();
+            //sql 작성
+            preparedStatement = connection.prepareStatement(
+                    "update userinfo set name = ?, password = ? where id = ?"
+            );
+            preparedStatement.setString(1, user.getName());
+            preparedStatement.setString(2, user.getPassword());
+            preparedStatement.setInt(3, user.getId());
+            //sql 실행
+            preparedStatement.executeUpdate();
+        } finally {
+            //자원 해지
+            try {
+                resultSet.close();
+            } catch (Exception throwables) {
+                throwables.printStackTrace();
+            }
+            try {
+                preparedStatement.close();
+            } catch (Exception throwables) {
+                throwables.printStackTrace();
+            }
+            try {
+                connection.close();
+            } catch (Exception throwables) {
+                throwables.printStackTrace();
+            }
+        }
+    }
 
+    public void delete(Integer id) throws SQLException {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        try {
+            //드라이버 로딩
+            connection = dataSource.getConnection();
+            //sql 작성
+            preparedStatement = connection.prepareStatement(
+                    "delete from userinfo where id = ?"
+            );
+            preparedStatement.setInt(1, id);
+
+            //sql 실행
+            preparedStatement.executeUpdate();
+        } finally {
+            //자원 해지
+            try {
+                resultSet.close();
+            } catch (Exception throwables) {
+                throwables.printStackTrace();
+            }
+            try {
+                preparedStatement.close();
+            } catch (Exception throwables) {
+                throwables.printStackTrace();
+            }
+            try {
+                connection.close();
+            } catch (Exception throwables) {
+                throwables.printStackTrace();
+            }
+        }
+    }
 }
 
 
