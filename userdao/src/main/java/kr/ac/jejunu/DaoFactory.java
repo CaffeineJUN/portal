9a98f35234aef9a0371428d8a528451152e9adc1
caffeinejun <gurwns232e@gmail.com>
2022-04-08 18:21:09 +0900

    0408 refactory

--- userdao/src/main/java/kr/ac/jejunu/DaoFactory.java
+++ userdao/src/main/java/kr/ac/jejunu/DaoFactory.java
@@ -6,35 +6,32 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 import javax.sql.DataSource;
-import java.io.PrintWriter;
-import java.sql.Connection;
 import java.sql.Driver;
-import java.sql.SQLException;
-import java.sql.SQLFeatureNotSupportedException;
-import java.util.logging.Logger;
 
 @Configuration
 public class DaoFactory {
     @Value("${db.drivername}")
-    private String driverClassName;
+    private String driverClassName; //= "com.mysql.cj.jdbc.Driver";
     @Value("${db.url}")
-    private String url;
+    private String url; //= "jdbc:mysql://192.168.151.176:3306/jeju?serverTimezone=UTC";
     @Value("${db.username}")
-    private String username;
+    private String username; //= "jeju";
     @Value("${db.password}")
-    private String password;
-
-
+    private String password; //= "jejupw";
     @Bean
     public UserDao userDao() throws ClassNotFoundException {
-        return new UserDao(dataSource());
+        return new UserDao(jdbcContext());
+    }
+
+    public JdbcContext jdbcContext() throws ClassNotFoundException {
+        return new JdbcContext(dataSource());
     }
 
     @Bean
     public DataSource dataSource() throws ClassNotFoundException {
         SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
-
-        dataSource.setDriverClass((Class<? extends Driver>) Class.forName(driverClassName));
+        dataSource.setDriverClass(
+                (Class<? extends Driver>) Class.forName(driverClassName));
         dataSource.setUrl(url);
         dataSource.setUsername(username);
         dataSource.setPassword(password);
--- userdao/src/main/java/kr/ac/jejunu/UserDao.java
+++ userdao/src/main/java/kr/ac/jejunu/UserDao.java
@@ -1,170 +1,38 @@
 package kr.ac.jejunu;
 
-import javax.sql.DataSource;
-import java.sql.*;
+import java.sql.SQLException;
 
 public class UserDao {
-    private final DataSource dataSource;
+    private final JdbcContext jdbcContext;
 
-    public UserDao(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public UserDao(JdbcContext jdbcContext) {
+        this.jdbcContext = jdbcContext;
     }
 
-    public User findById(Integer id) throws ClassNotFoundException, SQLException {
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-        ResultSet resultSet = null;
-        User user = null;
-        try{
-            connection = dataSource.getConnection();
-            //sql 작성
-            preparedStatement = connection.prepareStatement(
-                    "select * from userinfo where id = ?"
-            );
-            preparedStatement.setInt(1, id);
-            //sql 실행
-            resultSet = preparedStatement.executeQuery();
-            if(resultSet.next()) {
-                //User 에 데이터 매핑
-                user = new User();
-                user.setId(resultSet.getInt("id"));
-                user.setName(resultSet.getString("name"));
-                user.setPassword(resultSet.getString("password"));
-            }
-        }finally {
-            //자원 해지
-            try {
-                resultSet.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            try {
-                preparedStatement.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            try {
-                connection.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        //User 리턴
-        return user;
+    public User findById(Integer id) throws SQLException {
+        String sql = "select * from userinfo where id = ?";
+        Object[] params = new Object[]{id};
+        return jdbcContext.find(sql, params);
     }
 
-
-    public void insert(User user) throws ClassNotFoundException, SQLException {
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-        ResultSet resultSet = null;
-        try {
-            //드라이버 로딩
-            connection = dataSource.getConnection();
-            //sql 작성
-            preparedStatement = connection.prepareStatement(
-                    "insert into userinfo(name, password) values ( ?, ? )"
-                    , Statement.RETURN_GENERATED_KEYS
-            );
-            preparedStatement.setString(1, user.getName());
-            preparedStatement.setString(2, user.getPassword());
-            //sql 실행
-            preparedStatement.executeUpdate();
-            resultSet = preparedStatement.getGeneratedKeys();
-            resultSet.next();
-            //User 에 데이터 매핑
-            user.setId(resultSet.getInt(1));
-        } finally {
-            //자원 해지
-            try {
-                resultSet.close();
-            } catch (Exception throwables) {
-                throwables.printStackTrace();
-            }
-            try {
-                preparedStatement.close();
-            } catch (Exception throwables) {
-                throwables.printStackTrace();
-            }
-            try {
-                connection.close();
-            } catch (Exception throwables) {
-                throwables.printStackTrace();
-            }
-        }
-
+    public void insert(User user) throws SQLException {
+        String sql = "insert into userinfo(name, password) values ( ?, ? )";
+        Object[] params = new Object[]{user.getName(), user.getPassword()};
+        jdbcContext.insert(user, sql, params);
     }
 
     public void update(User user) throws SQLException {
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-        ResultSet resultSet = null;
-        try {
-            //드라이버 로딩
-            connection = dataSource.getConnection();
-            //sql 작성
-            preparedStatement = connection.prepareStatement(
-                    "update userinfo set name = ?, password = ? where id = ?"
-            );
-            preparedStatement.setString(1, user.getName());
-            preparedStatement.setString(2, user.getPassword());
-            preparedStatement.setInt(3, user.getId());
-            //sql 실행
-            preparedStatement.executeUpdate();
-        } finally {
-            //자원 해지
-            try {
-                resultSet.close();
-            } catch (Exception throwables) {
-                throwables.printStackTrace();
-            }
-            try {
-                preparedStatement.close();
-            } catch (Exception throwables) {
-                throwables.printStackTrace();
-            }
-            try {
-                connection.close();
-            } catch (Exception throwables) {
-                throwables.printStackTrace();
-            }
-        }
+        String sql = "update userinfo set name = ?, password = ? where id = ?";
+        Object[] params = new Object[]{user.getName(), user.getPassword(), user.getId()};
+        jdbcContext.update(sql, params);
     }
 
     public void delete(Integer id) throws SQLException {
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-        ResultSet resultSet = null;
-        try {
-            //드라이버 로딩
-            connection = dataSource.getConnection();
-            //sql 작성
-            preparedStatement = connection.prepareStatement(
-                    "delete from userinfo where id = ?"
-            );
-            preparedStatement.setInt(1, id);
-
-            //sql 실행
-            preparedStatement.executeUpdate();
-        } finally {
-            //자원 해지
-            try {
-                resultSet.close();
-            } catch (Exception throwables) {
-                throwables.printStackTrace();
-            }
-            try {
-                preparedStatement.close();
-            } catch (Exception throwables) {
-                throwables.printStackTrace();
-            }
-            try {
-                connection.close();
-            } catch (Exception throwables) {
-                throwables.printStackTrace();
-            }
-        }
+        String sql = "delete from userinfo where id = ?";
+        Object[] params = new Object[]{id};
+        jdbcContext.update(sql, params);
     }
+
 }
 
 
