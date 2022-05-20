57fa99e1509d590766f9679224ca9275ab9e7a2a
caffeinejun <gurwns232e@gmail.com>
2022-05-20 18:22:24 +0900

    spring boot init

---
+++ user2/src/test/java/kr/ac/jejunu/user2/User2ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.jejunu.user2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class User2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
